f419444f98c2c82c344a2e591ea51d40e23613fa
Erickbut <erickbutron1@gmail.com>
2023-12-18 00:22:04 -0400

    put in spanish the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,13 +19,13 @@ export default function Header() {
                 </form>
                 <ul className='flex gap-4'>
                     <Link to={'/'}>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Inicio</li>
                     </Link>
                     <Link to={'/about'}>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Sobre Nosotros</li>
                     </Link>
                     <Link to={'/sign-in'}>
-                        <li className='text-slate-700 hover:underline'>Sign In</li>
+                        <li className='text-slate-700 hover:underline'>Ingresar</li>
                     </Link>
                 </ul>
             </div>
