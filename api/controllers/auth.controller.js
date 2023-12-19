bd30c960f974ec82ab8692370fce6935d773f9ec
Erickbut <erickbutron1@gmail.com>
2023-12-18 23:14:04 -0400

    Complete Sign in page funcionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,7 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, 'Usuario no encontrado!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Credenciales incorrectos'));
+        if (!validPassword) return next(errorHandler(401, 'Credenciales incorrectas'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const { password: pass, ...rest } = validUser._doc;
         res
