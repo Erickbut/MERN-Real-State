92b9d668c923a6881d5a39b8f87779b849ebec40
Erickbut <erickbutron1@gmail.com>
2023-12-28 23:22:55 -0400

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Conectado a MongoDB')
-}).catch((err) => {
-    console.log(err);
-});
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Conectado a MongoDB')
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server corriendo en el puerto 3000');
