f589395af2df12afba405668aa9930c1fcefd9ab
Erickbut <erickbutron1@gmail.com>
2024-01-03 17:43:20 -0400

    complete update user funcionality with Mongo DB, google update have some errors

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,15 +9,13 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)
-        return next(errorHandler(401, 'Solo puedes actualizar tu propia cuenta.'));
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'Solo puedes actualizar tu propia cuenta.'));
     try {
         if (req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
 
-        const updatedUser = await User.findByIdAndUpdate(
-            req.params.id,
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
             {
                 $set: {
                     username: req.body.username,
@@ -25,9 +23,7 @@ export const updateUser = async (req, res, next) => {
                     password: req.body.password,
                     avatar: req.body.avatar,
                 },
-            },
-            { new: true }
-        );
+            }, { new: true });
 
         const { password, ...rest } = updatedUser._doc;
 
